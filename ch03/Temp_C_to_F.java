d529a3ef11855b02497854fffd45f15cdfe7d84f
QixinC <37123920+QixinC@users.noreply.github.com>
2018-03-12 19:20:04 -0500

    Update Temp_C_to_F.java

--- ch03/Temp_C_to_F.java
+++ ch03/Temp_C_to_F.java
@@ -1,19 +1,21 @@
-/**
-*Exercise 3-2
-*/
+import java.util.Scanner;
 
+
+/**
+
+*Exercise 3-2
+
+*/
+ 
 public class Temp_C_to_F {
-	
-	public static void main(String[] arg) {
-		
-		double C = 24.0;
-		double F = C*(9.0/5) + 32;
-		
-		System.out.printf("%.1f C = %.1f F", C, F);
-		
-		
-		
-	}
-	
-	
-}
+ 
+ public static void main(String[] arg) {
+  double C, F;
+  Scanner in = new Scanner(System.in);
+  
+  //promt the user to enter value
+  System.out.print("Please enter the Celsius:");
+  C = in.nextDouble();    
+  F = C*(9.0/5) + 32;
+ 
+  System.out.printf("%.1f C = %.1f F\n", C, F);
